f435b7c047fe4a60bc51fb75ba8bfd7e48146233
root <root@viola>
2021-05-23 23:37:12 -0700

    Some helpful erroring

--- ceph_from_kernel.sh
+++ ceph_from_kernel.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 
-ROOT=/usr/src/linux-$(uname -r)/
+ROOT=/usr/src/linux-$(uname -r)
+if [ ! -d ${ROOT} ]; then
+  echo ""
+  echo "***"
+  echo "*** FAILED to find linux kernel source code at ${ROOT} ***"
+  echo "***"
+  exit 1
+fi
 
 mkdir -p src/net/ceph src/fs/ceph
-cp -R ${ROOT}net/ceph/* src/net/ceph
-cp -R ${ROOT}fs/ceph/* src/fs/ceph
+cp -R ${ROOT}/net/ceph/* src/net/ceph
+cp -R ${ROOT}/fs/ceph/* src/fs/ceph

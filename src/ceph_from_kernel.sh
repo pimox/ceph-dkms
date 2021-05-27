274eb54c16a3a0e0ee347b989d5742fb8da39afe
root <root@viola>
2021-05-26 18:58:13 -0700

    Added rbd driver

--- src/ceph_from_kernel.sh
+++ src/ceph_from_kernel.sh
@@ -1,11 +1,14 @@
 #! /bin/sh
 
+DROOT="default-src"
 ROOT="/usr/src/linux-$(uname -r)"
 if [ ! -d ${ROOT} ]; then
   echo "*** Failed to find linux kernel source code at ${ROOT} - using default ***"
-  ROOT="default-src"
+  ROOT=$DROOT
 fi
 
-mkdir -p src/net/ceph src/fs/ceph
+mkdir -p src/net/ceph src/fs/ceph src/drivers/block
 cp -R ${ROOT}/net/ceph/* src/net/ceph
 cp -R ${ROOT}/fs/ceph/* src/fs/ceph
+cp -R ${ROOT}/drivers/block/rbd* src/drivers/block
+cp -R ${DROOT}/drivers/block/Makefile src/drivers/block
